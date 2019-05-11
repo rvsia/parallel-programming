8863daefc8f48544833e201d30ad4445e5c1b27c
Richard Všianský <32869456+rvsia@users.noreply.github.com>
2019-05-11 19:33:32 +0200

    Update multicore.py

--- multicore.py
+++ multicore.py
@@ -23,7 +23,6 @@ def cosine_dic(dic1,dic2):
 # mutexes
 
 mutex = Semaphore(1)
-mutexCompared = Semaphore(1)
 mutexPickle = Semaphore(1)
 
 # reading datasets
@@ -41,15 +40,13 @@ for index, code in enumerate(codes):
 # preparing shared array, the integer is count of documents in the dataset 
 
 uncompared = Array('i', 30)
-compared = Array('i', 30)
 
 # marking all documents as uncompared (because we could use only integers, 40 is used)
 
 for ind, code in enumerate(codes_id):
     uncompared[ind] = code
-    compared[ind] = 40
 
-def comparison(uncompared, compared):
+def comparison(uncompared):
     global codes_id
     first = -1
     should_continue = True # are some uncompared documents?
@@ -58,38 +55,31 @@ def comparison(uncompared, compared):
         # find uncompared document
         with mutex:
             for index in range(0,30):
-                if uncompared[index] < 40:
+                if uncompared[index] < 40 and first == -1: # najdeme prvni nejmensi cislo
                     first = copy.copy(uncompared[index])
             uncompared[first] = 40
             print(first)
 
         if first != -1: # uncompared document found
             for second in codes_id:
-                mutexCompared.acquire()
-                if not second in compared: # the second document is uncompared
-                    mutexCompared.release()
+                if second > first: # porovnáme s dokumenty pouze s vyšším id (nebyly porovnané ještě)
                     print(str(first) + ' >> ' + str(second))
-                    if first != second:
-                        t0 = time.time()
-                        result = token_comparison(codes_id[first], codes_id[second], 3, threshold=0.3,
-                                                  compare_function=cosine_dic)
-                        t1 = time.time()
-                        print(t1 - t0)
-                        with mutexPickle:
-                            with open('documents_gst_results03_pickle.dictionary', 'a+') as documents_pickle:
-                                pickle.dump({str(first) + '||' + str(second): result}, documents_pickle)
-                else:
-                    mutexCompared.release()
-            with mutexCompared:
-                compared[first] = first
+                    t0 = time.time()
+                    result = token_comparison(codes_id[first], codes_id[second], 3, threshold=0.3,
+                                              compare_function=cosine_dic)
+                    t1 = time.time()
+                    print(t1 - t0)
+                    with mutexPickle:
+                        with open('documents_gst_results03_pickle.dictionary', 'a+') as documents_pickle:
+                            pickle.dump({str(first) + '||' + str(second): result}, documents_pickle)
         else: # no uncompared document left
             should_continue = False
         first = -1
 
 if __name__ == "__main__":
-    process1 = Process(target=comparison, args=[uncompared, compared])
-    process2 = Process(target=comparison, args=[uncompared, compared])
-    process3 = Process(target=comparison, args=[uncompared, compared])
+    process1 = Process(target=comparison, args=[uncompared])
+    process2 = Process(target=comparison, args=[uncompared])
+    process3 = Process(target=comparison, args=[uncompared])
 
     process1.start()
     process2.start()
